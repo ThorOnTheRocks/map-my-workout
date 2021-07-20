ba45a33e469b9280de6220381cc74c39e47d6f63
gianluca galota <gianluca.galota@gmail.com>
2021-07-20 14:08:56 +0200

    set marker on map when click

--- script.js
+++ script.js
@@ -15,7 +15,32 @@ navigator.geolocation.getCurrentPosition(
   (pos) => {
     const { latitude } = pos.coords;
     const { longitude } = pos.coords;
-    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    const map = L.map('map').setView([latitude, longitude], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    map.on('click', (mapEvent) => {
+      console.log(mapEvent);
+      const { lat, lng } = mapEvent.latlng;
+
+      L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+          L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoclose: false,
+            closeOnClick: false,
+            className: 'running-popup'
+          })
+        )
+        .setPopupContent('Workout')
+        .openPopup();
+    })
+
+
   },
   () => {
     alert('Sorry, we were not able to find your position');
